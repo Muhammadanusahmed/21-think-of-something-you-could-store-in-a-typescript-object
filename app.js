1fce71501c48ce12c09ffa10ea53f0e66dfdd0fa
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-23 18:06:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var book = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    yearPublished: 1937
+};
+console.log("Book Info: ".concat(book.title, " is written by ").concat(book.author, ", published in ").concat(book.yearPublished));
+// Question22: intentional error
+var friends = ["Alice", "Bob", "Charlie"];
+console.log(friends[3]); // Intentional error: Arrays are zero-indexed, so index 3 is out of bounds.
+friends[2] = "Charlie"; // Correcting the error by accessing a valid index.
+console.log(friends[2]);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let book: { title: string; author: string; yearPublished: number } = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    yearPublished: 1937
+};
+console.log(`Book Info: ${book.title} is written by ${book.author}, published in ${book.yearPublished}`);
+
